f60e40484195241df4d7017bd5b30fae69f8a857
Máté Wolfram <mwolfram@users.noreply.github.com>
2017-08-31 23:36:36 +0200

    remove kitti data and test video

--- prep_aws.sh
+++ prep_aws.sh
@@ -4,20 +4,21 @@ pip install moviepy
 sudo apt-get install ffmpeg
 
 # get project
-git clone https://github.com/mwolfram/CarND-Semantic-Segmentation.git
+git clone https://github.com/mwolfram/CarND-Traffic-Light-Detection.git
+cd CarND-Semantic-Segmentation
 
 # prep kitti data
-cd CarND-Semantic-Segmentation
-cd data
-wget http://kitti.is.tue.mpg.de/kitti/data_road.zip
-unzip data_road.zip
+#cd data
+#wget http://kitti.is.tue.mpg.de/kitti/data_road.zip
+#unzip data_road.zip
 
-mkdir test_videos
-cd test_videos
-wget https://www.dropbox.com/s/9iiy7u4nv5l77ww/hart1.mp4?dl=1
+# get test video
+#mkdir test_videos
+#cd test_videos
+#wget https://www.dropbox.com/s/9iiy7u4nv5l77ww/hart1.mp4?dl=1
 
-cd ..
-cd ..
+#cd ..
+#cd ..
 
 # run project
 python main.py
